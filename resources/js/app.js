359e46a6ce62a73f2fef58e9c7827b1bb13ca8a6
rukaachan <novops911@gmail.com>
2023-10-19 13:02:53 +0700

    update:remove

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,5 @@ import "./bootstrap";
 import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
 
-import DataTable from "datatables.net-bs5";
-window.DataTable = DataTable;
-
 import Swal from "sweetalert2";
 window.swal = Swal;
