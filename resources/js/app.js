fc1c9c2912c1df965f34e6ef13d911463853997f
rukaachan <novops911@gmail.com>
2023-10-19 13:00:58 +0700

    added:jquery,sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+import DataTable from "datatables.net-bs5";
+window.DataTable = DataTable;
+
+import Swal from "sweetalert2";
+window.swal = Swal;
