dab0d3515a2abc2379612880b2d0dbe9319d0989
hrry6 <harrysiloam06@gmail.com>
2023-10-19 09:36:14 +0700

    Menambahkan asset image form login

--- app/Http/Controllers/OtentikasiController.php
+++ app/Http/Controllers/OtentikasiController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class OtentikasiController extends Controller
 {
-    //
+    /**
+     * Menampilkan halaman login.
+     */
+    public function index()
+    {
+        return view("Login.login");
+    }
+
+    /**
+     * Melakukan otentikasi login.
+     */
+    public function authenticated()
+    {
+        //
+    }
+
+    /**
+     * Melakukan fungsi logout
+     */
+    public function logout(Request $request)
+    {
+        //
+    }
 }
+
