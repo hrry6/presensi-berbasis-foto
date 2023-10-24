678821624259100d1fcc2854ac98f149e3047ff6
rukaachan <novops911@gmail.com>
2023-10-24 08:54:44 +0700

    update:stored procedure

--- database/migrations/2023_10_21_174107_stored_procedure.php
+++ database/migrations/2023_10_21_174107_stored_procedure.php
@@ -16,85 +16,94 @@ return new class extends Migration
         DB::unprepared('DROP Procedure IF EXISTS CreateGuruPiket');
         DB::unprepared('DROP Procedure IF EXISTS CreateWaliKelas');
         DB::unprepared('DROP Procedure IF EXISTS CreateAkunSiswa');
-        DB::unprepared("
-        CREATE PROCEDURE CreateGuruBK(
-            IN new_id_akun INT,
-            IN new_nama_guru VARCHAR(255),
-            IN new_foto_guru VARCHAR(255),
-            IN new_pembuat VARCHAR(255)
-        )
-        BEGIN
-            DECLARE new_id_guru INT;
-        
-            INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
-            VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
-        
-            SET new_id_guru = LAST_INSERT_ID();  
 
-            INSERT INTO guru_bk (id_guru) VALUES (new_id_guru);
-        END
-        ");
 
-        DB::unprepared("
-        CREATE PROCEDURE CreateGuruPiket(
-            IN new_id_akun INT,
-            IN new_nama_guru VARCHAR(255),
-            IN new_foto_guru VARCHAR(255),
-            IN new_pembuat VARCHAR(255)
-        )
-        BEGIN
-            DECLARE new_id_guru INT;
-        
-            INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
-            VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
-        
-            SET new_id_guru = LAST_INSERT_ID();  
-
-            INSERT INTO guru_piket (id_guru) VALUES (new_id_guru);
-        END
-        ");
-
-        DB::unprepared("
-        CREATE PROCEDURE CreateWaliKelas(
-            IN new_id_akun INT,
-            IN new_nama_guru VARCHAR(255),
-            IN new_foto_guru VARCHAR(255),
-            IN new_pembuat VARCHAR(255),
-            IN new_id_kelas INT
-        )
-        BEGIN
-            DECLARE new_id_wali_kelas INT;
-        
-            INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
-            VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
-        
-            SET new_id_wali_kelas = LAST_INSERT_ID();  
-
-            UPDATE kelas SET id_wali_kelas = new_id_wali_kelas WHERE id_kelas = new_id_kelas;
-        END
-        ");
-
-        DB::unprepared("
-        CREATE PROCEDURE CreateAkunSiswa(
-            IN p_id_siswa INT,
-            IN p_username VARCHAR(60),
-            IN p_password VARCHAR(225)
-        )
-        BEGIN
-            DECLARE p_id_akun INT;
+        DB::beginTransaction();
+        try {
+            DB::unprepared("
+            CREATE PROCEDURE CreateGuruBK(
+                IN new_id_akun INT,
+                IN new_nama_guru VARCHAR(255),
+                IN new_foto_guru VARCHAR(255),
+                IN new_pembuat VARCHAR(255)
+            )
+            BEGIN
+                DECLARE new_id_guru INT;
             
-            -- Insert data akun
-            INSERT INTO akun (id_role, username, password)
-            VALUES (1, p_username, p_password);
+                INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
+                VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
             
-            SET p_id_akun = LAST_INSERT_ID();  -- Mendapatkan ID akun yang baru dibuat
+                SET new_id_guru = LAST_INSERT_ID();  
+    
+                INSERT INTO guru_bk (id_guru) VALUES (new_id_guru);
+            END
+            ");
+
+            DB::unprepared("
+            CREATE PROCEDURE CreateGuruPiket(
+                IN new_id_akun INT,
+                IN new_nama_guru VARCHAR(255),
+                IN new_foto_guru VARCHAR(255),
+                IN new_pembuat VARCHAR(255)
+            )
+            BEGIN
+                DECLARE new_id_guru INT;
             
-            -- Update siswa dengan ID akun yang baru
-            UPDATE siswa
-            SET id_akun = p_id_akun
-            WHERE id_siswa = p_id_siswa;
-        END 
-        ");
+                INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
+                VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
+            
+                SET new_id_guru = LAST_INSERT_ID();  
+    
+                INSERT INTO guru_piket (id_guru) VALUES (new_id_guru);
+            END
+            ");
+
+            DB::unprepared("
+            CREATE PROCEDURE CreateWaliKelas(
+                IN new_id_akun INT,
+                IN new_nama_guru VARCHAR(255),
+                IN new_foto_guru VARCHAR(255),
+                IN new_pembuat VARCHAR(255),
+                IN new_id_kelas INT
+            )
+            BEGIN
+                DECLARE new_id_wali_kelas INT;
+            
+                INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
+                VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
+            
+                SET new_id_wali_kelas = LAST_INSERT_ID();  
+    
+                UPDATE kelas SET id_wali_kelas = new_id_wali_kelas WHERE id_kelas = new_id_kelas;
+            END
+            ");
+
+            DB::unprepared("
+            CREATE PROCEDURE CreateAkunSiswa(
+                IN p_id_siswa INT,
+                IN p_username VARCHAR(60),
+                IN p_password VARCHAR(225)
+            )
+            BEGIN
+                DECLARE p_id_akun INT;
+                
+                -- Insert data akun
+                INSERT INTO akun (id_role, username, password)
+                VALUES (1, p_username, p_password);
+                
+                SET p_id_akun = LAST_INSERT_ID();  -- Mendapatkan ID akun yang baru dibuat
+                
+                -- Update siswa dengan ID akun yang baru
+                UPDATE siswa
+                SET id_akun = p_id_akun
+                WHERE id_siswa = p_id_siswa;
+            END 
+            ");
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+        }
     }
 
     /**
@@ -102,9 +111,16 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::unprepared('DROP Procedure IF EXISTS CreateGuruBK');
-        DB::unprepared('DROP Procedure IF EXISTS CreateGuruPiket');
-        DB::unprepared('DROP Procedure IF EXISTS CreateWaliKelas');
-        DB::unprepared('DROP Procedure IF EXISTS CreateAkunSiswa');
+        DB::beginTransaction();
+        try {
+            DB::unprepared('DROP Procedure IF EXISTS CreateGuruBK');
+            DB::unprepared('DROP Procedure IF EXISTS CreateGuruPiket');
+            DB::unprepared('DROP Procedure IF EXISTS CreateWaliKelas');
+            DB::unprepared('DROP Procedure IF EXISTS CreateAkunSiswa');
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+        }
+
     }
 };
