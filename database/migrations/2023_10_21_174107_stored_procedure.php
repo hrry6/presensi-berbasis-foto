ee611dec51b38d8377040538d5cdd9ca4e7f0054
hrry6 <harrysiloam06@gmail.com>
2023-10-24 11:12:21 +0700

    Perbaikan commit & rollback

--- database/migrations/2023_10_21_174107_stored_procedure.php
+++ database/migrations/2023_10_21_174107_stored_procedure.php
@@ -17,93 +17,142 @@ return new class extends Migration
         DB::unprepared('DROP Procedure IF EXISTS CreateWaliKelas');
         DB::unprepared('DROP Procedure IF EXISTS CreateAkunSiswa');
 
-
-        DB::beginTransaction();
-        try {
-            DB::unprepared("
-            CREATE PROCEDURE CreateGuruBK(
-                IN new_id_akun INT,
-                IN new_nama_guru VARCHAR(255),
-                IN new_foto_guru VARCHAR(255),
-                IN new_pembuat VARCHAR(255)
-            )
+        DB::unprepared("
+        CREATE PROCEDURE CreateGuruBK(
+            IN new_id_akun INT,
+            IN new_nama_guru VARCHAR(255),
+            IN new_foto_guru VARCHAR(255),
+            IN new_pembuat VARCHAR(255)
+        )
+        BEGIN
+            DECLARE new_id_guru INT;
+            DECLARE pesan_error CHAR(5) DEFAULT '000';
+        
+            DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
             BEGIN
-                DECLARE new_id_guru INT;
-            
-                INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
-                VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
-            
-                SET new_id_guru = LAST_INSERT_ID();  
-    
-                INSERT INTO guru_bk (id_guru) VALUES (new_id_guru);
-            END
-            ");
-
-            DB::unprepared("
-            CREATE PROCEDURE CreateGuruPiket(
-                IN new_id_akun INT,
-                IN new_nama_guru VARCHAR(255),
-                IN new_foto_guru VARCHAR(255),
-                IN new_pembuat VARCHAR(255)
-            )
+                SET pesan_error = '001';
+            END;
+        
+            START TRANSACTION; -- Start the transaction
+        
+            INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
+            VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
+        
+            SET new_id_guru = LAST_INSERT_ID();
+        
+            INSERT INTO guru_bk (id_guru) VALUES (new_id_guru);
+        
+            IF pesan_error = '000' THEN
+                COMMIT; -- Commit the transaction if no error occurs
+            ELSE
+                ROLLBACK; -- Rollback the transaction if an error occurs
+            END IF;
+        END
+        ");
+        
+        DB::unprepared("
+        CREATE PROCEDURE CreateGuruPiket(
+            IN new_id_akun INT,
+            IN new_nama_guru VARCHAR(255),
+            IN new_foto_guru VARCHAR(255),
+            IN new_pembuat VARCHAR(255)
+        )
+        BEGIN
+            DECLARE new_id_guru INT;
+            DECLARE pesan_error CHAR(5) DEFAULT '000';
+        
+            DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
             BEGIN
-                DECLARE new_id_guru INT;
-            
-                INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
-                VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
-            
-                SET new_id_guru = LAST_INSERT_ID();  
-    
-                INSERT INTO guru_piket (id_guru) VALUES (new_id_guru);
-            END
-            ");
+                SET pesan_error = '001';
+            END;
+        
+            START TRANSACTION; -- Start the transaction
+        
+            INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
+            VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
+        
+            SET new_id_guru = LAST_INSERT_ID();  
 
-            DB::unprepared("
-            CREATE PROCEDURE CreateWaliKelas(
-                IN new_id_akun INT,
-                IN new_nama_guru VARCHAR(255),
-                IN new_foto_guru VARCHAR(255),
-                IN new_pembuat VARCHAR(255),
-                IN new_id_kelas INT
-            )
+            INSERT INTO guru_piket (id_guru) VALUES (new_id_guru);
+
+            IF pesan_error = '000' THEN
+                COMMIT; -- Commit the transaction if no error occurs
+            ELSE
+                ROLLBACK; -- Rollback the transaction if an error occurs
+            END IF;
+        END
+        ");
+
+        DB::unprepared("
+        CREATE PROCEDURE CreateWaliKelas(
+            IN new_id_akun INT,
+            IN new_nama_guru VARCHAR(255),
+            IN new_foto_guru VARCHAR(255),
+            IN new_pembuat VARCHAR(255),
+            IN new_id_kelas INT
+        )
+        BEGIN
+            DECLARE new_id_wali_kelas INT;
+            DECLARE pesan_error CHAR(5) DEFAULT '000';
+        
+            DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
             BEGIN
-                DECLARE new_id_wali_kelas INT;
+                SET pesan_error = '001';
+            END;
+        
+            START TRANSACTION; -- Start the transaction
             
-                INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
-                VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
-            
-                SET new_id_wali_kelas = LAST_INSERT_ID();  
-    
-                UPDATE kelas SET id_wali_kelas = new_id_wali_kelas WHERE id_kelas = new_id_kelas;
-            END
-            ");
+            INSERT INTO guru (id_akun, nama_guru, foto_guru, pembuat)
+            VALUES (new_id_akun, new_nama_guru, new_foto_guru, new_pembuat);
+        
+            SET new_id_wali_kelas = LAST_INSERT_ID();  
 
-            DB::unprepared("
-            CREATE PROCEDURE CreateAkunSiswa(
-                IN p_id_siswa INT,
-                IN p_username VARCHAR(60),
-                IN p_password VARCHAR(225)
-            )
+            UPDATE kelas SET id_wali_kelas = new_id_wali_kelas WHERE id_kelas = new_id_kelas;
+
+            IF pesan_error = '000' THEN
+                COMMIT; -- Commit the transaction if no error occurs
+            ELSE
+                ROLLBACK; -- Rollback the transaction if an error occurs
+            END IF;
+        END
+        ");
+
+        DB::unprepared("
+        CREATE PROCEDURE CreateAkunSiswa(
+            IN p_id_siswa INT,
+            IN p_username VARCHAR(60),
+            IN p_password VARCHAR(225)
+        )
+        BEGIN
+            DECLARE p_id_akun INT;
+
+            DECLARE pesan_error CHAR(5) DEFAULT '000';
+        
+            DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
             BEGIN
-                DECLARE p_id_akun INT;
-                
-                -- Insert data akun
-                INSERT INTO akun (id_role, username, password)
-                VALUES (1, p_username, p_password);
-                
-                SET p_id_akun = LAST_INSERT_ID();  -- Mendapatkan ID akun yang baru dibuat
-                
-                -- Update siswa dengan ID akun yang baru
-                UPDATE siswa
-                SET id_akun = p_id_akun
-                WHERE id_siswa = p_id_siswa;
-            END 
-            ");
+                SET pesan_error = '001';
+            END;
+        
+            START TRANSACTION; -- Start the transaction
+            
+            -- Insert data akun
+            INSERT INTO akun (id_role, username, password)
+            VALUES (1, p_username, p_password);
+            
+            SET p_id_akun = LAST_INSERT_ID();  -- Mendapatkan ID akun yang baru dibuat
+            
+            -- Update siswa dengan ID akun yang baru
+            UPDATE siswa
+            SET id_akun = p_id_akun
+            WHERE id_siswa = p_id_siswa;
 
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollback();
-        }
+            IF pesan_error = '000' THEN
+                COMMIT; -- Commit the transaction if no error occurs
+            ELSE
+                ROLLBACK; -- Rollback the transaction if an error occurs
+            END IF;
+        END 
+        ");
     }
 
     /**
@@ -111,16 +160,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::beginTransaction();
-        try {
-            DB::unprepared('DROP Procedure IF EXISTS CreateGuruBK');
-            DB::unprepared('DROP Procedure IF EXISTS CreateGuruPiket');
-            DB::unprepared('DROP Procedure IF EXISTS CreateWaliKelas');
-            DB::unprepared('DROP Procedure IF EXISTS CreateAkunSiswa');
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollback();
-        }
-
+        DB::unprepared('DROP Procedure IF EXISTS CreateGuruBK');
+        DB::unprepared('DROP Procedure IF EXISTS CreateGuruPiket');
+        DB::unprepared('DROP Procedure IF EXISTS CreateWaliKelas');
+        DB::unprepared('DROP Procedure IF EXISTS CreateAkunSiswa');
     }
 };
