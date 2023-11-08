6da13e59870657c0ddad3d82313bd4a3755a1ca3
rukaachan <novops911@gmail.com>
2023-11-08 12:32:28 +0700

    update:change migration pengurus kelas

--- database/migrations/2023_10_13_201433_create_pengurus_kelas_table.php
+++ database/migrations/2023_10_13_201433_create_pengurus_kelas_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('pengurus_kelas', function (Blueprint $table) {
             $table->integer('id_pengurus', true);
             $table->integer('id_siswa');
-            $table->string('jabatan', 20);
-            $table->enum('jabatan', ['sekretaris','ketuakelas','wakilkelas', 'siswa']);
             $table->string('pembuat', 60);
             
             // Foreign Key
